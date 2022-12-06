8984872eb7db7c784f5ec14fde8dc81886d03a29
Louis Shinohara <louishinohara@gmail.com>
2022-12-06 08:57:25 -0500

    day6

--- __init__.py
+++ __init__.py
@@ -3,13 +3,15 @@ from src.days.day2 import day2
 from src.days.day3 import day3
 from src.days.day4 import day4
 from src.days.day5 import day5
+from src.days.day6 import day6
 
 def main():
     # day1.exec()
     # day2.exec()
     # day3.exec()
     # day4.exec()
-    day5.exec()
+    # day5.exec()
+    day6.exec()
 
 
 main()
